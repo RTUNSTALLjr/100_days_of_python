958b0629afb0466901a0f809b22758c78a3234de
Richard Tunstall <ricktunstalljr@gmail.com>
2023-04-22 23:46:45 -0400

    first commit

---
+++ day_1/bandname_generator.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator')
+
+city = input('What city were you born in ? ')
+pet = input('What is the name of your pet ? ')
+
+print(f'Your band name could be {city} {pet}')
